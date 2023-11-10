9343af6fdb7ed9ce7f61a892f4bcca7f3c001028
creatorm22 <mikautadze.giorgi2223@gmail.com>
2023-11-10 16:48:29 +0400

    removed quotes

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main>
-      <h2>Welcome to "ნამიოკობანა"</h2>
+      <h2>Welcome to ნამიოკობანა</h2>
       <h3>How to play</h3>
       <p>
         You will be asked total of 5 questions. In each question there will a
